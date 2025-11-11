ca408a94c293ceda811609c22a64bdaa95b4cb52
CamilaCampos <camiicampos411@gmail.com>
2025-11-11 19:58:34 -0300

    Agregados estilos negro y plateado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import NotFound from "./components/NotFound";
+import './App.css';
 
 function App() {
   return (
