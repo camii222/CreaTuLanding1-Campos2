6c304d992e7ab944125df60a00e35864f32edb00
CamilaCampos <camiicampos411@gmail.com>
2025-11-11 20:18:06 -0300

    ultimos detalles

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,13 @@
-import Item from "./Item";
-
 export default function ItemList({ products }) {
   return (
     <div className="item-list">
-      {products.map(p => (
-        <Item key={p.id} product={p} />
+      {products.map((p) => (
+        <div className="item-card" key={p.id}>
+          <img src={p.image} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+          <button>Ver detalle</button>
+        </div>
       ))}
     </div>
   );
