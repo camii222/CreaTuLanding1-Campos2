4ae8e9641ea4cde624a054bd9b933cd2f6b7d964
CamilaCampos <camiicampos411@gmail.com>
2025-11-11 21:10:29 -0300

    entrega final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import NotFound from "./components/NotFound";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter basename="/CreaTuLanding1+Campos2">
+    <>
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContainer />} />
@@ -15,8 +15,6 @@ function App() {
         <Route path="/item/:id" element={<ItemDetailContainer />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
-
-export default App;
